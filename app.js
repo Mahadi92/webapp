fe13ddc6c43b5cf7c87aa7be31dab10a53054d19
sumuhridoy2002 <sumuhridoy2002@gmail.com>
2022-05-06 22:44:40 +0600

    Web App

---
+++ app.js
@@ -0,0 +1,50 @@
+const videos = [
+    {
+        subject: 'Physics',
+        chapter: 'chapter 1',
+        name: 'video 1',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Physics',
+        chapter: 'chapter 1',
+        name: 'video 2',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Chemistry',
+        chapter: 'chapter 1',
+        name: 'video 1',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Chemistry',
+        chapter: 'chapter 1',
+        name: 'video 2',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Math',
+        chapter: 'chapter 1',
+        name: 'video 1',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Math',
+        chapter: 'chapter 1',
+        name: 'video 2',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Biology',
+        chapter: 'chapter 1',
+        name: 'video 1',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    },
+    {
+        subject: 'Biology',
+        chapter: 'chapter 1',
+        name: 'video 2',
+        url: 'https://www.youtube.com/embed/zxJ32Y_tVp0'
+    }
+]
